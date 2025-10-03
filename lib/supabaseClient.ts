a660d6c6f611b2ee074256c44ce7b57467bdbcb5
yusufkurnia <42245260+yusufkurnia@users.noreply.github.com>
2025-10-03 14:05:14 +0700

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// Ambil dari environment variables (sudah kamu set di Vercel)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
+// Buat client Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
