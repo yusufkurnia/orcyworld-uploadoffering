1d56dc053625482b41cd00aaffb40029981d26e5
yusufkurnia <42245260+yusufkurnia@users.noreply.github.com>
2025-10-03 16:19:14 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,40 @@ import { UploadOffering } from "@/components/upload-offering"
 export default function Page() {
   return (
     <main className="min-h-dvh relative flex flex-col items-center">
-      {/* Corner accents */}
-      <img src="/images/top-left.png" alt="" aria-hidden="true" className="pointer-events-none select-none fixed left-2 top-2 w-24 md:w-36 opacity-90" />
-      <img src="/images/top-right.png" alt="" aria-hidden="true" className="pointer-events-none select-none fixed right-2 top-2 w-24 md:w-36 opacity-90" />
-      <img src="/images/bottom-left.png" alt="" aria-hidden="true" className="pointer-events-none select-none fixed left-2 bottom-2 w-24 md:w-36 opacity-90" />
-      <img src="/images/bottom-right.png" alt="" aria-hidden="true" className="pointer-events-none select-none fixed right-2 bottom-2 w-24 md:w-36 opacity-90" />
+      {/* Corner accents - decorative */}
+      <img
+        src="/images/top-left.png"
+        alt=""
+        aria-hidden="true"
+        className="pointer-events-none select-none fixed left-2 top-2 w-24 md:w-36 opacity-90"
+      />
+      <img
+        src="/images/top-right.png"
+        alt=""
+        aria-hidden="true"
+        className="pointer-events-none select-none fixed right-2 top-2 w-24 md:w-36 opacity-90"
+      />
+      <img
+        src="/images/bottom-left.png"
+        alt=""
+        aria-hidden="true"
+        className="pointer-events-none select-none fixed left-2 bottom-2 w-24 md:w-36 opacity-90"
+      />
+      <img
+        src="/images/bottom-right.png"
+        alt=""
+        aria-hidden="true"
+        className="pointer-events-none select-none fixed right-2 bottom-2 w-24 md:w-36 opacity-90"
+      />
 
-      {/* Logo */}
-      <img src="/images/logo.jpeg" alt="Ritual seal" className="fixed top-8 left-1/2 -translate-x-1/2 w-40 md:w-56 h-auto select-none pointer-events-none" />
+      {/* Logo (fixed, centered) */}
+      <img
+        src="/images/logo.jpeg"
+        alt="Ritual seal"
+        className="fixed top-20 left-1/2 -translate-x-1/2 w-40 md:w-56 h-auto select-none pointer-events-none"
+      />
 
-      {/* Upload Widget */}
+      {/* UploadOffering renders the yellow button below logo + scrollable list */}
       <UploadOffering />
     </main>
   )
